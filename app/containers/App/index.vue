d6e7e1f97da418e047cee778e0688bc97e067fae
Jeison Borja <Jeison.Borja@endava.com>
2018-04-16 13:54:02 -0500

    Changed iView to Vuetify

--- app/containers/App/index.vue
+++ app/containers/App/index.vue
@@ -1,32 +1,54 @@
 <template>
   <div id="app" class="layout">
-    <Layout>
-      <Header :style="{height: '10vh'}">
-        <div class="logo">
-          <router-link to="/">
-            <img src="/assets/VueBoilerplate.png" />
-          </router-link>
-        </div>
-      </Header>
-      <Layout :style="{minHeight: '90vh'}">
-        <Sider
-          collapsible
-          :collapsed-width="78"
-          v-model="isCollapsed"
-          :style="{background: '#fff'}"
-        >
-          <SidebarMenu v-bind="{isCollapsed}" />
-        </Sider>
-        <Layout>
-          <Content :style="{minHeight: '80vh', padding: '3vh 3vw'}">
-            <router-view/>
-          </Content>
-          <Footer :style="{minHeight: '3vh'}">
-            Vue-boilerplate - 2018
-          </Footer>
-        </Layout>
-      </Layout>
-    </Layout>
+    <v-app id="inspire">
+      <v-navigation-drawer
+        fixed
+        app
+      >
+        <v-list dense>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Home</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Contact</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar color="indigo" dark fixed app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid fill-height>
+          <v-layout
+            justify-center
+            align-center
+          >
+            <v-flex text-xs-center>
+              <v-tooltip left>
+                <v-btn icon large :href="source" target="_blank" slot="activator">
+                  <v-icon large>code</v-icon>
+                </v-btn>
+                <span>Source</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <v-footer color="indigo" app inset>
+        <span class="white--text">&copy; 2017</span>
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
