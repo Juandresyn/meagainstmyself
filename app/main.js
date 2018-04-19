84ff64fbf220c183f9c0f2675db76ad652341c27
Nicolas Arteaga <narteaganieto@mcclatchyservices.com>
2018-04-19 11:55:52 -0500

    Themifying with vuetify

--- app/main.js
+++ app/main.js
@@ -5,9 +5,10 @@ import VueRouter from 'vue-router';
 import VueFastAxios from 'vue-fast-axios';
 import VueHead from 'vue-head';
 import VueLogger from 'vuejs-logger';
-import vuetify from 'vuetify';
+import Vuetify from 'vuetify';
 import VueI18n from 'vue-i18n';
 import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors';
 
 import App from './containers/App';
 import router from './router';
@@ -18,8 +19,18 @@ import messages from './translations';
 Vue.use(VueRouter);
 Vue.use(VueFastAxios);
 Vue.use(VueHead);
-Vue.use(vuetify);
 Vue.use(VueI18n);
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.indigo.darken4,
+    secondary: colors.indigo.lighten1,
+    accent: colors.red.base,
+    success: colors.green.darken2,
+    error: colors.red.darken2,
+    warning: colors.amber.darken1,
+    info: colors.blue.darken2,
+  },
+});
 Vue.use(VueLogger, config.loggerOptions);
 
 const i18n = new VueI18n({
